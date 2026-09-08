a9f1897fc00970a1477361e7425ea70a0f7fe1f0
brmkruse <brmkruse@gmail.com>
2026-09-07 21:43:59 -0700

    XP: fix corrupted usf-bulls asset

--- src/assets/usf-bulls.ts
+++ src/assets/usf-bulls.ts
@@ -1,1 +1,3 @@
-PLACEHOLDER_LOAD_FROM_DISK
+/** Athletic USF Bulls logo for Education (Wikimedia commons thumb). */
+export const usfBullsSrc =
+  'https://upload.wikimedia.org/wikipedia/commons/thumb/1/13/Official_USF_Bulls_Athletic_Logo.png/200px-Official_USF_Bulls_Athletic_Logo.png';
