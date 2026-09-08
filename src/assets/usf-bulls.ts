cb50fcc58cafdd985af26c42858a53a25aa31ee5
brmkruse <brmkruse@gmail.com>
2026-09-07 21:55:06 -0700

    XP: self-host USF Bulls logo as data URI

--- src/assets/usf-bulls.ts
+++ src/assets/usf-bulls.ts
@@ -1,3 +1,1 @@
-/** Athletic USF Bulls logo for Education (Wikimedia commons thumb). */
-export const usfBullsSrc =
-  'https://upload.wikimedia.org/wikipedia/commons/thumb/1/13/Official_USF_Bulls_Athletic_Logo.png/200px-Official_USF_Bulls_Athletic_Logo.png';
+PLACEHOLDER_LOAD_FROM_FILE
